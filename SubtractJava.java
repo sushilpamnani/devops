274e5d1d4f4c7c57216c5fc6bdf7113af2560cc1
Sushil Pamnani <55614913+sushilpamnani@users.noreply.github.com>
2022-04-29 11:10:02 +0530

    Update SubtractJava.java

--- SubtractJava.java
+++ SubtractJava.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {  
 int n1 = 225, n2 = 115, difference;  
 difference = n1 - n2;  
-System.out.println("The difference of" + n1 + " and " + n2 + " is: "+difference );  
+System.out.println("The difference of " + n1 + " and " + n2 + " is: "+difference );  
 }  
 }  
